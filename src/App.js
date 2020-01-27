d81a8f4651532fd31980f18e3dc32f4c80e03f21
Danika Thomson <danikathomson@Danikas-mac.local>
2020-01-27 15:36:29 +0100

    retrieved data from Nasa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import ComponentCardContainer from "./Components/ComponentCardContainer";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=TvE1IYeov47cGJDDL9v0WPeGUkRHTfQeZ3tupeAR")
+    .then(response => {
+       setData(response.data);
+      console.log(response.data);
+    })
+
+  .catch(error => {
+    console.log("Uh ohhh! Where's my data??!!", error);
+});
+
+}, []);
+
   return (
     <div className="App">
       <ComponentCardContainer />
