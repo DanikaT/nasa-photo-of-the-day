3566fe6a1175bde4edf5728593e2ee5179b015aa
Danika Thomson <danikathomson@Danikas-mac.local>
2020-01-27 15:17:21 +0100

    Added card component and card container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import ComponentCardContainer from "./Components/ComponentCardContainer";
 
 function App() {
   return (
     <div className="App">
+      <ComponentCardContainer />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/Components/ComponentCardContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ComponentCard from "./ComponentCard";
+
+function ComponentCardContainer() {
+    return (
+      <div className="App">
+          <ComponentCard />
+        <p>
+          Read through the instructions in the README.md file to build your NASA
+          app! Have fun 🚀!
+        </p>
+      </div>
+    );
+  }
+  
+  export default ComponentCardContainer;
